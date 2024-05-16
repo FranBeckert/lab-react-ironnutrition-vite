4d6f8385c08ba4b3a76688103cf2285cf691b30c
Franciely Beckert <francielybeckert@gmail.com>
2024-05-16 16:23:05 +0200

    Do up to up to 4

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,22 @@
 // Your code here
+function FoodBox({ food, deleteFood}) {
+    
+  return (
+    <div style={{border:"1px solid gray"}}>
+      <p>{food.name}</p>
+
+      <img src={food.image}  style={{width:150}}/>
+
+      <p>Calories: {food.calories}</p>
+      <p>Servings {food.servings}</p>
+
+      <p>
+        <b>Total Calories: {food.servings * food.calories} </b> kcal
+      </p>
+
+      <button onClick={()=> deleteFood(food.id)} >Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox;
